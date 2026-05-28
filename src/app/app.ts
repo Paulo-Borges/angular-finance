8818d41ad4498ed6b702ea40952675314a93b6a5
Borges <borgespaulo72@yahoo.com.br>
2026-05-28 13:56:51 -0300

    refactor: refatorando Header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FinanceService } from './service/finance.service';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
